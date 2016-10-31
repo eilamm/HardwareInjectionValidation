0b8bdd3b1353195fe6be85fdb0f5873847140ace
eilamm <eilam@umich.edu>
2016-10-30 22:04:48 -0400

    Believe I fixed the bug in discardLines. Changed sod variable in Pulsar.getData() as well

--- discardLines.m
+++ discardLines.m
@@ -3,7 +3,7 @@
 % discardLines: discards n numberr of lines while reading a
 % text file
 function discardLines(fileID, n)
-    for i=0:1:n
-        temp = fgetl(fileID)
+    for i=1:1:n
+        fgetl(fileID);
     end
 end
