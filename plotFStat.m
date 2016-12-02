b5ad284a2788c9fb28ad54f74149c73d8e5f558e
eilamm <eilam@umich.edu>
2016-12-02 13:00:54 -0500

    Cleaning up and removing code duplication in plotFStat using new function plotFStat_helper

--- plotFStat.m
+++ plotFStat.m
@@ -45,36 +45,38 @@ function plotFStat(A, B, id)
    end
    
    % This will serve as the xaxis values, to clean up the code.
-   xaxis = 1:1:num_days;
+%    xaxis = 1:1:num_days;
    
    % Plot cumulative data %%%%%%%%%%%%%%%%%%%%%%%
-   figure;
-   % Plot the computed values
-   plot(xaxis, cumulData(index, 1), '-o', xaxis, cumulData(index, 2), '-x');
-%    hold on;
-   % Plot the predicted values on the same graph
-%    plot(xaxis, cumulData(index, 2), '-x');
-   legend('Computed', 'Predicted');
-   title(['Cumulative Fstat Values between ', A.date2str(), ' and ', B.date2str(), ' for Pulsar ', num2str(id)]);
-   xlabel(['Days since ', A.date2str()]);
-   ylabel('Fstat');
-   filename = sprintf('%s%i%s%s%s%s%s', '/home/eilam.morag/public_html/HWInjection/Pulsar_', id, '_', A.date2str_nospace(), '_', B.date2str_nospace(), '_c.png');
-   saveas(gcf, filename);
-   close(gcf);
+   plotFStat_helper(A, B, cumulData, id, cumulative);
+%    figure;
+%    % Plot the computed values
+%    plot(xaxis, cumulData(index, 1), '-o', xaxis, cumulData(index, 2), '-x');
+% %    hold on;
+%    % Plot the predicted values on the same graph
+% %    plot(xaxis, cumulData(index, 2), '-x');
+%    legend('Computed', 'Predicted');
+%    title(['Cumulative Fstat Values between ', A.date2str(), ' and ', B.date2str(), ' for Pulsar ', num2str(id)]);
+%    xlabel(['Days since ', A.date2str()]);
+%    ylabel('Fstat');
+%    filename = sprintf('%s%i%s%s%s%s%s', '/home/eilam.morag/public_html/HWInjection/Pulsar_', id, '_', A.date2str_nospace(), '_', B.date2str_nospace(), '_c.png');
+%    saveas(gcf, filename);
+%    close(gcf);
    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    
    % Plot daily data %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-   figure;
-   % Plot the computed values
-   plot(xaxis, dailyData(index, 1), '-o', xaxis, dailyData(index, 2), '-x');
-%    hold on;
-   % Plot the predicted values on the same graph
-%    plot(xaxis, dailyData(index, 2), '-x');
-   legend('Computed', 'Predicted');
-   title(['Daily Fstat Values between ', A.date2str(), ' and ', B.date2str(), ' for Pulsar ', num2str(id)]);
-   xlabel(['Days since ', A.date2str()]);
-   ylabel('Fstat');
-   filename = sprintf('%s%i%s%s%s%s%s', '/home/eilam.morag/public_html/HWInjection/Pulsar_', id, '_', A.date2str_nospace(), '_', B.date2str_nospace(), '_d.png');
-   saveas(gcf, filename);
-   close(gcf);
+   plotFStat_helper(A, B, dailyData, id, daily);
+%    figure;
+%    % Plot the computed values
+%    plot(xaxis, dailyData(index, 1), '-o', xaxis, dailyData(index, 2), '-x');
+% %    hold on;
+%    % Plot the predicted values on the same graph
+% %    plot(xaxis, dailyData(index, 2), '-x');
+%    legend('Computed', 'Predicted');
+%    title(['Daily Fstat Values between ', A.date2str(), ' and ', B.date2str(), ' for Pulsar ', num2str(id)]);
+%    xlabel(['Days since ', A.date2str()]);
+%    ylabel('Fstat');
+%    filename = sprintf('%s%i%s%s%s%s%s', '/home/eilam.morag/public_html/HWInjection/Pulsar_', id, '_', A.date2str_nospace(), '_', B.date2str_nospace(), '_d.png');
+%    saveas(gcf, filename);
+%    close(gcf);
 end
---
+++ plotFStat_helper.m
@@ -0,0 +1,32 @@
+% Eilam Morag
+% December 2, 2016
+% plotFStat_helper: Plots the data and sets appropriate titles and labels.
+
+function plotFStat_helper(A, B, data, id, cumulative)
+    num_days = A - B;
+    xaxis = 1:1:num_days;
+    
+    figure;
+    
+    % Plot the computed values
+    scatter(xaxis, data(:, 1), '-o', 'red');
+    hold on;
+    
+    % Plot the predicted values on the same graph
+    scatter(xaxis, data(:, 2), '-o', 'blue');
+    
+    legend('Computed', 'Predicted');
+    title(['Daily Fstat Values between ', A.date2str(), ' and ', B.date2str(), ' for Pulsar ', num2str(id)]);
+    xlabel(['Days since ', A.date2str()]);
+    ylabel('Fstat');
+    
+    filename = sprintf('%s%i%s%s%s%s%s', '/home/eilam.morag/public_html/HWInjection/Pulsar_', id, '_', A.date2str_nospace(), '_', B.date2str_nospace());
+    if (cumulative == 0)
+        filename = [filename, '_d.png'];
+    elseif (cumulative == 1)
+        filename = [filename, '_c.png'];
+    end
+    saveas(gcf, filename);
+    
+    close(gcf);
+end
