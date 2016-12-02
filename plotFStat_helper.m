9b57dab7ad1420943bd10b06dfc1b2c725be46aa
eilamm <eilam@umich.edu>
2016-12-02 13:10:40 -0500

    Fixed plot title bug

--- plotFStat_helper.m
+++ plotFStat_helper.m
@@ -15,18 +15,18 @@ function plotFStat_helper(A, B, data, id, cumulative)
     % Plot the predicted values on the same graph
     scatter(xaxis, data(:, 2), 'blue');
     
-    legend('Computed', 'Predicted');
-    title(['Daily Fstat Values between ', A.date2str(), ' and ', B.date2str(), ' for Pulsar ', num2str(id)]);
-    xlabel(['Days since ', A.date2str()]);
-    ylabel('Fstat');
-    
     filename = sprintf('%s%i%s%s%s%s%s', '/home/eilam.morag/public_html/HWInjection/Pulsar_', id, '_', A.date2str_nospace(), '_', B.date2str_nospace());
     if (cumulative == 0)
         filename = [filename, '_d.png'];
+        title(['Daily Fstat Values between ', A.date2str(), ' and ', B.date2str(), ' for Pulsar ', num2str(id)]);
     elseif (cumulative == 1)
         filename = [filename, '_c.png'];
+        title(['Cumulative Fstat Values between ', A.date2str(), ' and ', B.date2str(), ' for Pulsar ', num2str(id)]);
     end
-    saveas(gcf, filename);
+    legend('Computed', 'Predicted');     
+    xlabel(['Days since ', A.date2str()]);
+    ylabel('Fstat');
     
+    saveas(gcf, filename);
     close(gcf);
 end
