1c3182b63727c3358fec71e155686c6ad04a9af6
eilamm <eilam@umich.edu>
2016-12-05 09:17:17 -0500

    plots over all of o1

--- plotFStat_drive.m
+++ plotFStat_drive.m
@@ -1,12 +1,12 @@
 % Eilam Morag
 % November 28, 2016
-% Calls plotFStat for different dates
+% Calls plotFStat for all pulsars
 
 clear
 close
-
-A = Date([1, 17, 2016]);
+A = Date([11, 25, 2015]);
+B = Date([1, 17, 2016]);
 
 for i=0:14
-    plotFStat(A, A, i);
+    plotFStat(A, B, i);
 end
