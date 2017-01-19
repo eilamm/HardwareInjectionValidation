9fb5a7c61367180213b3179f373c2c4beacf4803
eilamm <eilam@umich.edu>
2017-01-19 09:09:12 -0500

    Fixed bug where the runall scripts were being output to the wrong folder (not the scripts one). This was a problem because the lalapps scripts that the runall scripts were calling referred to the datafiles as simply the symlinks/*.sft (e.g. '011317/*.sft' instead of 'scripts/011317/*.sft') which meant that when the runallscripts were run from the Hardware_Injection_2016 directory, that made it the base directory, and so when the lalapps scripts looked for the datafiles they were nowhere to be found. No idea how the system worked before.

--- create_runAll_scripts_helper.m
+++ create_runAll_scripts_helper.m
@@ -8,7 +8,7 @@ function create_runAll_scripts_helper(inputFiles, outputFile)
     file_list = {folder.name}';
     nrows = length(file_list);
     
-    fullpath = ['/home/eilam.morag/hw_injection/Hardware_Injection_2016/', outputFile];
+    fullpath = ['/home/eilam.morag/hw_injection/Hardware_Injection_2016/scripts/', outputFile];
     fileID = fopen(outputFile, 'w');
     
     script_beginning = sprintf('%s\n\n%s\n%s\n\n', '#!/bin/bash',...
@@ -19,7 +19,7 @@ function create_runAll_scripts_helper(inputFiles, outputFile)
 
 
     for row = 1:nrows
-        fprintf(fileID, '%s%s\n', './scripts/', file_list{row, :});
+        fprintf(fileID, '%s%s\n', './', file_list{row, :});
     end
     
     script_ending = sprintf('\n\n%s\n\n%s\n%s\n%s\n', 'echo "Finished running all scripts"', ...
