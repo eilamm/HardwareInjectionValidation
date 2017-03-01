fd29295f01cac2a9e9ee620b61de6b045c4ef2db
eilamm <eilam@umich.edu>
2017-02-28 23:23:54 -0500

    fixed bug

--- plotFStat_drive.m
+++ plotFStat_drive.m
@@ -8,13 +8,12 @@ A = Date([11, 25, 2015]);
 B = Date([12, 3, 2015]);
 
 %% If the HWInjection folder does not exist, create it
-temp = 'home/eilam.morag/public_html/HWInjection';
-folder = dir(temp);
-if (isempty(folder))
-    disp(['Creating the folder: ', temp]);
-    mkdir(temp);
+folder = 'home/eilam.morag/public_html/HWInjection';
+if (~exist(folder, 'dir'))
+    disp(['Creating the folder: ', folder]);
+    mkdir(folder);
 end
-clear temp folder
+clear folder
 
 %% Call plotFStat for each pulsar
 for i=0:14
