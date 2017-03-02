bf8381ec77cd71b829e30533136fede1b2b65647
eilamm <eilam@umich.edu>
2017-03-01 20:03:06 -0500

    Attempted fix with readableDate.js

--- changeDate.js
+++ changeDate.js
@@ -8,4 +8,4 @@ function changeDate(date) {
       var plot_d = folder.concat("_d.png");
       document.getElementById("plot_c").src = plot_c;
       document.getElementById("plot_d").src = plot_d;
-};
+      };
