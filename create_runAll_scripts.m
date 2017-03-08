6aa5a167072d9a69732c5e3bfd9bce3ed71b73b7
eilamm <eilam@umich.edu>
2017-03-08 09:56:39 -0500

    Bug fixes

--- create_runAll_scripts.m
+++ create_runAll_scripts.m
@@ -46,7 +46,7 @@ end
 function create_runAll_scripts_helper(list, outputFile)
     nrows = length(list);
     
-    base = 'home/eilam.morag/hw_injection/Hardware_Injection_2016/scripts/';
+    base = '/home/eilam.morag/hw_injection/Hardware_Injection_2016/scripts/';
     
     outpath = [base, outputFile];
     fileID = fopen(outpath, 'w');
@@ -80,8 +80,8 @@ end
 % Returns a list of scriptnames that fall within the constraints and have
 % not yet been executed
 function list = listUnexecutedScripts(startdate, enddate, compute, daily)
-    outpath = '/home/eilam.morag/hw_injection/Hardware_Injection_2016/output/';
-    scrpath = '/home/eilam.morag/hw_injection/Hardware_Injection_2016/scripts/';
+    outpath = 'home/eilam.morag/hw_injection/Hardware_Injection_2016/output/';
+    scrpath = 'home/eilam.morag/hw_injection/Hardware_Injection_2016/scripts/';
     %% Pre and suffixes to the Fstat files and lalapps scripts
     % Example file: FstatValues_9_Nov-30-2015_daily.txt; FstatLoudest_9_Jan-6-2016_cumulative.txt
     % Example script: predict_pulsarx9_Dec-9-2015_daily; recover_pulsarx2_Dec-21-2015_daily
@@ -108,9 +108,9 @@ function list = listUnexecutedScripts(startdate, enddate, compute, daily)
     while (d <= enddate)
         date = d.date2str_nospace();
         for ID = pulsars
-            file = sprintf('%s%d%s', outpath, fileprefix, ID, '_', date, filesuffix);
+            file = sprintf('%s%s%d%s%s%s', outpath, fileprefix, ID, '_', date, filesuffix);
             if (~exist(file, 'file'))
-                script = sprintf('%s%d%s', scrpath, scriptprefix, ID, '_', date, scriptsuffix);
+                script = sprintf('%s%s%d%s%s%s', scrpath, scriptprefix, ID, '_', date, scriptsuffix);
                 list{i} = script;
                 i = i + 1;
             end
