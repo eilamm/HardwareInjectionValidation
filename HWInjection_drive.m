a3a129359e2855c5fc7e16bf9073772299225761
eilamm <eilam@umich.edu>
2017-03-08 23:27:14 -0500

    Changed dates

--- HWInjection_drive.m
+++ HWInjection_drive.m
@@ -15,7 +15,7 @@ disp('Inputted pulsars: ');
 disp(pulsar_list_IDs);
 
 start_today = Date([11, 25, 2015]); % Start of O1
-end_today = Date([12, 4, 2015]);
+end_today = Date([12, 31, 2015]);
 % end_today = Date([1, 17, 2016]); % End of O1
 
 % start_today = Date([12, 31, 2016]);
