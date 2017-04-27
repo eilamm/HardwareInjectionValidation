5da0318f055a1443039e06395aef09daeeefd976
eilamm <eilam@umich.edu>
2017-04-27 13:35:41 -0400

    Updated some graphics in the vlines function, and investigating livingston disappearing data bug

--- plot_vlines.m
+++ plot_vlines.m
@@ -1,12 +1,13 @@
-% TURNS Hold on
+% Plots vertical dashed lines to denote the month ends in the range
+% contained by startDate and endDate
+% NOTE: Turns "hold" on
 function plot_vlines(startDate, endDate)
     hold on;
     day = startDate;
     i = 0;
     while (day <= endDate)
         if (day.last_of_month() == 1)
-            line([i i], ylim, 'LineStyle', '--', 'Color', 'm', ...
-                 'LineWidth', 0.85);
+            line([i i], ylim, 'LineStyle', '--');
         end
         day = day.next_day();
         i = i + 1;
