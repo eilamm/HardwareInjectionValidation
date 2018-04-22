cebd12bcca960e15fb3cbe4110a0f1c3f16b72ad
Eilam Morag <eilam.morag@ligo.org>
2018-04-22 16:36:56 -0700

    Adding includePaths.m

---
+++ includePaths.m
@@ -0,0 +1,7 @@
+% This script adds the Functions, Classes, Plotting, Parsing, and Scripts directories to the path
+addpath('/home/eilam.morag/hw_injection/Hardware_Injection_2016/Functions');
+addpath('/home/eilam.morag/hw_injection/Hardware_Injection_2016/Scripts');
+addpath('/home/eilam.morag/hw_injection/Hardware_Injection_2016/Classes');
+addpath('/home/eilam.morag/hw_injection/Hardware_Injection_2016/Plotting');
+addpath('/home/eilam.morag/hw_injection/Hardware_Injection_2016/Parsing');
+addpath('/home/eilam.morag/hw_injection/Hardware_Injection_2016/Website');
