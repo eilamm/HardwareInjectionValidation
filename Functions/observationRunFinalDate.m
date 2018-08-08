246ce5e237f4b40f99fc2f220177a1de0321f74e
Eilam Morag <eilam.morag@ligo.org>
2018-08-07 21:31:46 -0700

    observationRunFinalDate will now return current date if final date in the observation settings file is set to 'auto'

--- Functions/observationRunFinalDate.m
+++ Functions/observationRunFinalDate.m
@@ -4,9 +4,13 @@ function fd = observationRunFinalDate()
     fileID = fopen(pathSettings);
     discardLines(fileID, 3);
     temp = strsplit(fgetl(fileID), '-');    
-    m = str2num(temp{1});
-    d = str2num(temp{2});
-    y = str2num(temp{3});
-    fd = Date([m, d, y]);
+    if (strcmpi(temp, 'auto'))
+        fd = todayDate();
+    else
+        m = str2num(temp{1});
+        d = str2num(temp{2});
+        y = str2num(temp{3});
+        fd = Date([m, d, y]);
+    end
     fclose(fileID);
 end
